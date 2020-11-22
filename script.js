3e3ddf229cd05791b0de8763d0f6404a232b6e1f
lbmann <l.bmann@yahoo.com>
2020-11-21 20:10:13 -0500

    organized + labeled css, as well as adding detailed comments about functionality in the js file

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+// preset variables
+
 var apiKey = "5a8ad98e6af76e72b4b654f3790e8fcd";
 var now =  moment().format("MM/DD/YYYY");
 var searchBar = $("#searchBar").val();
 
+
+// variables to be set by js/user input
 
 var city;
 var cityLon;
@@ -15,6 +19,8 @@ var weatherIconURL;
 var citiesSearched = [];
 
 
+// on page load, update the cities searched array to match the localstorage array. if there are cities to display, display them in the list
+    // and provide a clear button. if not, move on.
 
 $(document).ready(function() {
 
@@ -53,16 +59,28 @@ $("body").keypress(function(e){
     }
 });
 
+
+// on click button listens for a click on anywhere on the code body, and only institutes an action if it has the "cityBtn" class
+
 $("body").on('click', function(event) {
+
+    // if what is clicked is a button
 
     if ($(event.target).hasClass("cityBtn")) {
 
+    // if the search input has no user input when a button is clicked, check for conditionals
+
     if ($("#searchBar").val() == "") {
+
+        // if the search input is empty, and the user clicked the search button, give an error
 
         if ($(event.target).attr("value") == undefined) {
             $("#error").text("You need to enter a city!");
             return;
         }
+        
+        // if the search input is empty, and the user clicked on a previously entered city in the history, load that city
+            // but do not re-add it to the array or to the list.
 
         else {
             $("#error").text("");
@@ -90,12 +108,17 @@ $("body").on('click', function(event) {
           
         city = capitalize(city);
 
+        // make sure if a city is entered, it hasn't already been entered before, after the input has been homogenized
+
         for (i=0; i<citiesSearched.length; i++) {
             if (citiesSearched[i] == city) {
                 $("#error").text("You've already added this city to the list!");
                 return;
             }
         }
+
+        // create a list item containing a button with the text and value of the city inputed
+                // append this button to the user history list underneath the search input
 
         var cityItemLi = $("<li>");
 
@@ -114,6 +137,8 @@ $("body").on('click', function(event) {
         citiesSearched.push(city);
         localStorage.setItem("cities", JSON.stringify(citiesSearched));
 
+        // update variable array and local storage array to both include the new city
+
         $("#clearResults").css("display", "block");
 
         pullCityCoord();
@@ -125,6 +150,8 @@ $("body").on('click', function(event) {
 });
 
 function pullCityCoord() {
+
+    // take the city name from the user input or from clicking a button of a previously searched city and pull the lat/lon of that city from the API
 
     $.ajax({
         url: "https://api.openweathermap.org/data/2.5/weather?q=" + city +"&appid=" + apiKey,
@@ -138,6 +165,8 @@ function pullCityCoord() {
 
 
 function pullWeatherInfo() {
+
+    // using the lat/lon values pulled from the last function, use another url with queries lat & lon to pull all weather data needed for the page
 
     $.ajax({
         url: "https://api.openweathermap.org/data/2.5/onecall?lat=" + cityLat + "&lon=" + cityLon + "&units=imperial&appid=" + apiKey,
@@ -161,6 +190,8 @@ function pullWeatherInfo() {
         $("#todayWind").text(cityWindSpeed);
 
         $("#todayUV").text(cityUV);
+
+        // color the UV variable based on severity
 
         if (cityUV < 3) {
             $("#todayUV").css("background-color", "rgb(0,128,0)");
@@ -201,6 +232,8 @@ function pullWeatherInfo() {
 
         $("#todayImg").attr("src", weatherIconURL);
 
+        // loop through the array of future days in the API and display the data/dates in the five-day forecast
+
         for (i=1; i<response.daily.length; i++) {
             var futureDayTitle = $("#day-" + i + " .futureDate");
             var futureDayDate = moment.unix(response.daily[i].dt).format("MM/DD/YYYY");
@@ -214,12 +247,16 @@ function pullWeatherInfo() {
             $("#day-" + i + " .futureHumidity").text(futureHumidity);
         }
 
+        // make the weather info visible, as it is defaulted to be hidden on page load until a user inputs a city
+
         $(".weather-information").css("visibility", "visible");
 
     });
 
 }
 
+// on click of the clear button, clears the variable and local storage arrays, as well as the user history list, and hides the clear button
+        // until a city is added to the list again to be cleared
 
 $("#clearResults").on('click', function(event) {
     localStorage.removeItem("cities");
